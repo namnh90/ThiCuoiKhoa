aa8576982bc1320dd2055f3d56996a89c99bf2a8
namnh90 <nam90.nguyenhoang@gmail.com>
2025-08-18 12:23:25 +0700

    update js

--- thicuoiky.js
+++ thicuoiky.js
@@ -51,14 +51,14 @@ function check_chien_thang() {
         if (Number(box.textContent) !== i + 1) { status = "false"; break }
     }
     if (status === "done") {
-         if (checkdemgio) {
+        if (checkdemgio) {
             clearInterval(checkdemgio);
             checkdemgio = null;
             count_time = doi_gio(sec);
             document.getElementsByClassName('btn-1-1-stop')[0].style.display = "none";
             document.getElementsByClassName('btn-1-1-start')[0].style.display = "";
             batdaugame = false;
-            document.getElementsByClassName('text_chien_thang')[0].style.display="block";
+            document.getElementsByClassName('text_chien_thang')[0].style.display = "block";
             taodivlichsu();
         }
     }
@@ -70,13 +70,42 @@ function shuffle() {
     const list_all_box = document.querySelectorAll('[class^="cell-2-1-1-box-game"]');
     let list_info_cell = []
     for (const box of list_all_box) {
-        list_info_cell.push({ so_thu_tu: box.textContent, color: box.style.backgroundColor, colortext: box.style.color })
+        list_info_cell.push({ so_thu_tu: Number(box.textContent), color: box.style.backgroundColor, colortext: box.style.color })
     };
-    for (let k = 0; k < 100; k++) {
-        const i = Math.floor(Math.random() * 12);
-        const j = Math.floor(Math.random() * 12);
-        [list_info_cell[i], list_info_cell[j]] = [list_info_cell[j], list_info_cell[i]];
+    function dem_so_nghich_dao(list_info_cell) {
+        let count_nghich_dao = 0;
+        const numbers = list_info_cell.filter(box => box.so_thu_tu !== 12);
+        for (let i = 0; i < numbers.length - 1; i++) {
+            for (let j = i + 1; j < numbers.length; j++) {
+                if (numbers[i].so_thu_tu > numbers[j].so_thu_tu) count_nghich_dao++;
+            }
+        }
+        return count_nghich_dao;
+
     };
+
+    function check_giai_duoc(list_info_cell) {
+        const count_nghich_dao = dem_so_nghich_dao(list_info_cell);
+        console.log(count_nghich_dao);
+        const vitri_black = list_info_cell.findIndex(box => box.so_thu_tu === 12);
+        console.log(vitri_black);
+        const rowFromBottom = 3 - Math.floor(vitri_black / 4);
+        console.log(rowFromBottom);
+        console.log((dem_so_nghich_dao(list_info_cell) + rowFromBottom) % 2 === 0)
+        return (dem_so_nghich_dao(list_info_cell) + rowFromBottom) % 2 === 0;
+
+    };
+    do {
+        for (let k = 0; k < 100; k++) {
+            const i = Math.floor(Math.random() * 12);
+            const j = Math.floor(Math.random() * 12);
+            [list_info_cell[i], list_info_cell[j]] = [list_info_cell[j], list_info_cell[i]];
+        }
+    }
+
+    while (check_giai_duoc(list_info_cell))
+        ;
+    console.log(!check_giai_duoc(list_info_cell));
     list_all_box.forEach((box, i) => {
         box.textContent = list_info_cell[i].so_thu_tu;
         box.style.backgroundColor = list_info_cell[i].color;
@@ -97,7 +126,7 @@ function doi_gio(sec) {
 document.getElementsByClassName('btn-1-1-start')[0].addEventListener('click', () => {
     document.getElementsByClassName('btn-1-1-start')[0].style.display = "none";
     document.getElementsByClassName('btn-1-1-stop')[0].style.display = "flex";
-    document.getElementsByClassName('text_chien_thang')[0].style.display="none";
+    document.getElementsByClassName('text_chien_thang')[0].style.display = "none";
     sec = 0;
     count_move = 1;
     batdaugame = true;
@@ -116,7 +145,7 @@ document.getElementsByClassName('btn-1-1-stop')[0].addEventListener('click', () 
         document.getElementsByClassName('timecount')[0].textContent = "00:00";
         document.getElementsByClassName('btn-1-1-stop')[0].style.display = "none";
         document.getElementsByClassName('btn-1-1-start')[0].style.display = "";
-        document.getElementsByClassName('text_chien_thang')[0].style.display="none";
+        document.getElementsByClassName('text_chien_thang')[0].style.display = "none";
     }
 })
 
@@ -126,7 +155,7 @@ function taodivlichsu() {
     div.style.maxWidth = "1800px";
     div.style.width = "100%";
     div.style.height = "50px";
-    div.style.backgroundColor =  "transparent";
+    div.style.backgroundColor = "transparent";
     div.style.display = "grid";
     div.style.gridTemplateColumns = "1fr 4fr 4fr";
     div.style.gap = "5px";
